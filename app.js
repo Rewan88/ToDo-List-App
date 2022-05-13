d81bc8b1427e49717a8acca869863b35497ba387
Rewan88 <92064063+Rewan88@users.noreply.github.com>
2022-05-13 16:52:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const morgan = require("morgan");
+
+require("dotenv").config();
+
+const app = express();
+
+// dotenv.config({ path : '.env'})
+
+const PORT = process.env.PORT || 4004;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+app.use("/api/todo", require("./routes/todo"));
+app.use('/user', '/routes/user.js');
+
+// log any error caused when connect to database
+db().catch((err) => console.log(err));
+
+// connect to database
+async function db() {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("**connected to db**");
+}
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to our ToDo List Application API");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Is Running on http://localhost:${PORT}`);
+});
